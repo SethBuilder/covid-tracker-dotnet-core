8ef0da0f74539d8474769abf9518a66597583d48
Seif <seif@seif.rock>
2020-03-17 17:02:18 +0200

    Updates

---
+++ precache-manifest.1fec1305f1e4f163f34510edb13540f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9549660ed98f184158cb1bbeb8d57f5a",
+    "url": "/covid/index.html"
+  },
+  {
+    "revision": "07eaff73f30882329fd7",
+    "url": "/covid/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "885acaa7652db9557adb",
+    "url": "/covid/static/css/main.13b298a8.chunk.css"
+  },
+  {
+    "revision": "07eaff73f30882329fd7",
+    "url": "/covid/static/js/2.67c400fa.chunk.js"
+  },
+  {
+    "revision": "885acaa7652db9557adb",
+    "url": "/covid/static/js/main.b7bdea6d.chunk.js"
+  },
+  {
+    "revision": "c2c03d96972f8e1c7e98",
+    "url": "/covid/static/js/runtime~main.afd96dac.js"
+  }
+]);
